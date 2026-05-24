3852236ebd3d8ca1fc284625ecac78be983de2bf
swarajko <swaraj.sk10@gmail.com>
2026-05-24 22:25:00 +0100

    Update activations.py

--- nn/activations.py
+++ nn/activations.py
@@ -5,8 +5,17 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+
 def relu(x):
     return np.maximum(0, x)
+
+
+def relu_derivative(x):
+    return (x > 0).astype(float)
 
 
 def softmax(x):
