3c7db1cba27075e5597cf9926d499edceba897e3
swarajko <swaraj.sk10@gmail.com>
2026-05-24 23:31:49 +0100

    Update network.py

--- nn/network.py
+++ nn/network.py
@@ -1,14 +1,30 @@
 import numpy as np
 
-from nn.activations import relu, relu_derivative, softmax
+from nn.activations import (
+    relu,
+    relu_derivative,
+    sigmoid,
+    sigmoid_derivative,
+    tanh,
+    tanh_derivative,
+    softmax
+)
 
 
 class FeedForwardNN:
 
-    def __init__(self, input_size, hidden_layers, output_size, activation="relu", weight_init="random"):
+    def __init__(
+        self,
+        input_size,
+        hidden_layers,
+        output_size,
+        activation="relu",
+        weight_init="random"
+    ):
 
         self.weights = []
         self.biases = []
+
         self.activation_name = activation
 
         layers = [input_size] + hidden_layers + [output_size]
@@ -16,14 +32,14 @@ class FeedForwardNN:
         for i in range(len(layers) - 1):
 
             if weight_init == "xavier":
-            
+
                 W = np.random.randn(
                     layers[i],
                     layers[i + 1]
                 ) * np.sqrt(1 / layers[i])
-            
+
             else:
-            
+
                 W = np.random.randn(
                     layers[i],
                     layers[i + 1]
@@ -33,77 +49,6 @@ class FeedForwardNN:
 
             self.weights.append(W)
             self.biases.append(b)
-
-    def forward(self, X):
-
-        self.activations = [X]
-        self.z_values = []
-
-        A = X
-
-        # hidden layers
-        for i in range(len(self.weights) - 1):
-
-            Z = np.dot(A, self.weights[i]) + self.biases[i]
-
-            self.z_values.append(Z)
-
-            A = self.activation(Z)
-
-            self.activations.append(A)
-
-        # output layer
-        Z = np.dot(A, self.weights[-1]) + self.biases[-1]
-
-        self.z_values.append(Z)
-
-        output = softmax(Z)
-
-        self.activations.append(output)
-
-        return output
-
-    def backward(self, X, y):
-
-        m = X.shape[0]
-
-        self.dW = [0] * len(self.weights)
-        self.db = [0] * len(self.biases)
-
-        # output layer gradient
-        dZ = self.activations[-1] - y
-
-        self.dW[-1] = np.dot(
-            self.activations[-2].T,
-            dZ
-        ) / m
-
-        self.db[-1] = np.sum(
-            dZ,
-            axis=0,
-            keepdims=True
-        ) / m
-
-        # hidden layers
-        for i in reversed(range(len(self.weights) - 1)):
-
-            dA = np.dot(
-                dZ,
-                self.weights[i + 1].T
-            )
-
-            dZ = dA * self.activation_derivative(self.z_values[i])
-
-            self.dW[i] = np.dot(
-                self.activations[i].T,
-                dZ
-            ) / m
-
-            self.db[i] = np.sum(
-                dZ,
-                axis=0,
-                keepdims=True
-            ) / m
 
     def activation(self, x):
 
@@ -126,3 +71,72 @@ class FeedForwardNN:
 
         elif self.activation_name == "tanh":
             return tanh_derivative(x)
+
+    def forward(self, X):
+
+        self.activations = [X]
+        self.z_values = []
+
+        A = X
+
+        for i in range(len(self.weights) - 1):
+
+            Z = np.dot(A, self.weights[i]) + self.biases[i]
+
+            self.z_values.append(Z)
+
+            A = self.activation(Z)
+
+            self.activations.append(A)
+
+        Z = np.dot(A, self.weights[-1]) + self.biases[-1]
+
+        self.z_values.append(Z)
+
+        output = softmax(Z)
+
+        self.activations.append(output)
+
+        return output
+
+    def backward(self, X, y):
+
+        m = X.shape[0]
+
+        self.dW = [0] * len(self.weights)
+        self.db = [0] * len(self.biases)
+
+        dZ = self.activations[-1] - y
+
+        self.dW[-1] = np.dot(
+            self.activations[-2].T,
+            dZ
+        ) / m
+
+        self.db[-1] = np.sum(
+            dZ,
+            axis=0,
+            keepdims=True
+        ) / m
+
+        for i in reversed(range(len(self.weights) - 1)):
+
+            dA = np.dot(
+                dZ,
+                self.weights[i + 1].T
+            )
+
+            dZ = dA * self.activation_derivative(
+                self.z_values[i]
+            )
+
+            self.dW[i] = np.dot(
+                self.activations[i].T,
+                dZ
+            ) / m
+
+            self.db[i] = np.sum(
+                dZ,
+                axis=0,
+                keepdims=True
+            ) / m
