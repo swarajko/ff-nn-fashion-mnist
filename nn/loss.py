40095ca78218c31827b6d3b5bfa989300b1b21f3
swarajko <swaraj.sk10@gmail.com>
2026-05-24 22:25:23 +0100

    Implement cross-entropy loss function

--- nn/loss.py
+++ nn/loss.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+
+def cross_entropy_loss(y_true, y_pred):
+
+    epsilon = 1e-10
+
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+
+    loss = -np.mean(
+        np.sum(y_true * np.log(y_pred), axis=1)
+    )
+
+    return loss
